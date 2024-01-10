f33d0c41ebee1b712a2c7b7a2596bc8384eb8207
573ned <573ned@gmail.com>
2024-01-10 13:27:45 +0300

    strong upod

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ def generate_simple_password():
     lowercase_letters = string.ascii_lowercase
     digits = string.digits
     
-    password_length = (8)
-    num_digits = random.randint(1, min(2, password_length - 1))
-    num_lowercase = password_length - num_digits
+    num_lowercase = 6
+    num_digits = 2
     
     password = (
         ''.join(random.choice(lowercase_letters) for _ in range(num_lowercase)) +
@@ -17,12 +16,13 @@ def generate_simple_password():
     return ''.join(random.sample(password, len(password)))
 
 def generate_strong_password():
-    characters = string.ascii_letters + string.digits + string.punctuation
+    characters = string.ascii_letters + string.digits + "/.,_"
     return ''.join(random.choice(characters) for _ in range(random.randint(9, 12)))
 
 def main():
-    print("1. Простой пароль (8 символов, строчные буквы и 1-2 цифры)")
-    print("2. Сложный пароль (9-12 символов с прописными, заглавными буквами, цифрами и спец символами)")
+    print("Выберите тип пароля:")
+    print("1. Простой пароль (8 символов, строчные буквы и 2 цифры)")
+    print("2. Сложный пароль (9-12 символов с прописными и строчными буквами, цифрами, спец символами: /, ., , или _)")
 
     choice = input("Введите номер выбора (1 или 2): ")
 
